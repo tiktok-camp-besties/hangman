a532914227889177779eba6566dcc8f82d179ddc
ian-from-dover <ian09hong@gmail.com>
2022-05-24 00:02:09 +0800

    GetRandWord: Separate JSX and logic more

--- src/components/GamePlayingPage.js
+++ src/components/GamePlayingPage.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'; // react will only re-render components when they update if they use a state.
 import '../assets/GamePlayingPage.css';
-import GetRandomWord from './GetRandomWord';
+import { getNewAnswer, CATEGORIES, setWordbank } from './GetRandomWord';
 
 
 /**
@@ -10,12 +10,69 @@ import GetRandomWord from './GetRandomWord';
  * @returns HTML of the playing page.
  */
 export default function GamePlayingPage({ changePageFn, allWords }) {
+    // ----- declarations ----- 
+
+    // tell React to update components when answer is changed.
+    const [answer, changeAnswer] = useState('');
+    const [category, changeCategory] = useState(''); // change this to the long-form category cuz the display will be of the long-form one
+    // also, the display needs to convert it to all caps
+    // qn: can a state change in a separate class update a component in this class? these 2 declarations really belong in getrandomword not here. The many functions should be moved over to that class
+
+    setWordbank(allWords);
+
+    function setNewAnswer() {
+        changeAnswer(prevAnswer => {
+            return getNewAnswer(category);
+        });
+    }
+
+    function setNewCategory(newCategory) {
+        if (CATEGORIES.has(newCategory)) {
+            changeCategory(prevCategory => {
+                return newCategory;
+            });
+        } else {
+            console.error("Tried to set an invalid category \""
+                + newCategory
+                + "\". You can only set choc, names, foreign or crush.");
+            return null;
+        }
+    }
+
+    function setCatChoc() {
+        setNewCategory("choc");
+    }
+
+    function setCatNames() {
+        setNewCategory("names");
+    }
+
+    function setCatForeign() {
+        setNewCategory("foreign");
+    }
+
+    function setCatCrush() {
+        setNewCategory("crush");
+    }
+
     return (
         <>
             <h1>Game Playing Page</h1>
             <div>We are now playing hangman!</div>
             <br />
-            <GetRandomWord wordBank={allWords} />
+
+            {/* random word displayer and controls */}
+            <div>Category: {category}</div>
+            <button onClick={() => setCatChoc()}>Change to choc category</button>
+            <button onClick={() => setCatNames()}>Change to names category</button>
+            <button onClick={() => setCatCrush()}>Change to crush category</button>
+            <button onClick={() => setCatForeign()}>Change to foreign category</button>
+            <br />
+            <button onClick={() => setNewAnswer()}>Generate next word</button>
+            <br />
+            <br />
+            <div>The answer is: {answer}</div>
+
             <br />
             <button onClick={() => changePageFn('ended')}>Finish Game</button>
         </>
--- src/components/GetRandomWord.js
+++ src/components/GetRandomWord.js
@@ -1,78 +1,107 @@
 import React, { useState } from 'react';
 
 /**
- * React does not know that it needs to re-render the answer component
- * unless I use the react state. So the displayed answer will only change when 
- * React watches for the state of the answer variable to change.
- * 
- * future plans for this function: export the get category function and the get rand word function,
- * create a new set category function and their enum-ed functions to call from other scripts.
- * stop returning JSX. this doesnt need to be html anymore.
+ * API:
+ * descriptive string getCurrCategory
+ * setCategory(string category) // triggered by button
+ * string getCurrAnswer
+ * string getNewAnswer
  */
 
+// ----- declarations ----- 
 
 /**
  * Used to get the correct words from the category in the bank.
  */
-const CATEGORIES = new Map([
+export const CATEGORIES = new Map([
     ["foreign", "Foreign words used in English"],
     ["names", "Gender neutral first names"],
     ["choc", "Things that go well with chocolate"],
     ["crush", "Things people do to impress their crush"]
 ]);
 
+// the current answer and category
+let answer, category, wordBank;
+
+// ----- private functions used internally ----- 
+
 /**
- * Contains all the functionality to randomly generate a word from the given category.
- * 
- * @param {prop[]} props - variables being passed into this component
- * @returns HTML to control the random generation of words. Also displays the word.
+ * Returns a random element from an array.
+    * Referenced from Corey on stack overflow
+    * https://stackoverflow.com/a/5915122/16777554
+    * 
+    * @param {*} array to pick the element from
+    */
+function getRandomElem(array) {
+    const randFloatingIndex = Math.random() * array.length;
+    const randIndex = Math.floor(randFloatingIndex);
+    return array[randIndex];
+}
+
+/**
+ * Sets the answer word to be a random word from the given category 
+    * 
+    * @param {string} category the category of the word, 
+    * which will be either "crush", "choc", "names", or "foreign"
+    */
+function getRandomWord(currCategory) {
+    console.log("inside getRandomWord function");
+    console.log(currCategory);
+    console.log(wordBank);
+    let newAnswer = getRandomElem(wordBank[CATEGORIES.get(currCategory)]); // something screwing up here
+
+}
+
+// ----- public functions used by other components ----- 
+
+/**
+ * Sets the word bank that random words will be picked from.
+ * @param {Object} newWordBank - the word bank object that the answers will be picked from
  */
-export default function GetRandomWord({ wordBank }) {
-    // ----- declarations ----- 
+export function setWordbank(newWordBank) {
+    wordBank = newWordBank;
+}
 
-    // tell React to update components when answer is changed.
-    const [answer, changeAnswer] = useState('');
+/**
+ * @returns {string} the current answer
+ */
+export function getCurrAnswer() {
+    return answer;
+}
 
-    // ----- functions ----- 
+/**
+ * Generates a new answer using the existing category.
+ * Plans: supposed to take in 0 parameters and use the state's category to change, but idk if i can do that and have the state's display element update as well.
+ * 
+ * @returns {string} the new answer
+ */
+export function getNewAnswer(thisCategory) {
+    answer = getRandomWord(thisCategory); // returning undefined
+    console.log(answer);
+    return answer;
+}
 
-    /**
-     * Returns a random element from an array.
-     * Referenced from Corey on stack overflow
-     * https://stackoverflow.com/a/5915122/16777554
-     * 
-     * @param {*} array to pick the element from
-     */
-    function getRandomElem(array) {
-        const randFloatingIndex = Math.random() * array.length;
-        const randIndex = Math.floor(randFloatingIndex);
-        return array[randIndex];
+/**
+ * @returns {string} the current category in its short form
+ */
+export function getCurrCategory() {
+    return category;
+}
+
+/**
+ * Sets the category of the answer words being generated.
+ * 
+ * @param {string} newCategory - either choc, names, foreign or crush.
+ * @returns {string} the new category in its short form
+ */
+export function setCategory(newCategory) {
+    if (CATEGORIES.has(newCategory)) {
+        category = newCategory;
+        return category;
+    } else {
+        console.error("Tried to set an invalid category \""
+            + newCategory
+            + "\". You can only set choc, names, foreign or crush.");
+        return null;
     }
-
-    /**
-     * Sets the answer word to be a random word from the given category 
-     * 
-     * @param {string} category the category of the word, 
-     * which will be either "crush", "choc", "names", or "foreign"
-     */
-    function getRandomWord(category) {
-        let newAnswer = getRandomElem(wordBank[CATEGORIES.get(category)]);
-
-        changeAnswer(prevAnswer => {
-            return newAnswer;
-        });
-    }
-
-    // ----- html of component -----
-    const currKey = "choc"; // let user choose in next iteration
-    return (
-        <>
-            <h2>Get Random Word Component</h2>
-            <div>Category: {CATEGORIES.get(currKey)}</div>
-            <br />
-            <button onClick={() => getRandomWord(currKey)}>Generate next word</button>
-            <br />
-            <br />
-            <div>The answer is: {answer}</div>
-        </>
-    );
 }
