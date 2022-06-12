afbb4d61e6eda8c77aced20e8d74e07fcae6eeb4
ian-from-dover <ian09hong@gmail.com>
2022-06-12 15:29:31 +0800

    Updates

---
+++ precache-manifest.cb8642eb1b7b9db8ecdc6322ddc06f7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fe786a8ac1f14e5175e",
+    "url": "/static/css/main.4ac1ac51.chunk.css"
+  },
+  {
+    "revision": "4fe786a8ac1f14e5175e",
+    "url": "/static/js/main.5aca9224.chunk.js"
+  },
+  {
+    "revision": "67e15eb299556ce4070a",
+    "url": "/static/js/runtime~main.0fe1a568.js"
+  },
+  {
+    "revision": "efd15f03ba6bbdeed640",
+    "url": "/static/js/2.7c631a53.chunk.js"
+  },
+  {
+    "revision": "db569b567d12d48e4e61",
+    "url": "/static/js/3.d51c813c.chunk.js"
+  },
+  {
+    "revision": "feecc1b482c49ca4150f033ef421e3da",
+    "url": "/static/media/HappyTreeFriends.feecc1b4.png"
+  },
+  {
+    "revision": "495e344a00f8509be251febb09d9eb28",
+    "url": "/static/media/bg.495e344a.png"
+  },
+  {
+    "revision": "723abb926f3f9d52ba8bdb8e22de5863",
+    "url": "/index.html"
+  }
+];
